4a6ba27735953cf3acb2b5e11323e1cf79d3032e
soyouda <soyouda@gmail.com>
2019-03-03 19:29:23 +0900

    API Registering 조회수

--- routes.js
+++ routes.js
@@ -6,7 +6,6 @@ const LOGOUT = "/logout";
 const SEARCH = "/search";
 
 // Users
-
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
@@ -14,7 +13,6 @@ const CHANGE_PASSWORD = "/change-password";
 const ME = "/me";
 
 // Videos
-
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
@@ -22,14 +20,17 @@ const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 // Github
-
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
-
 
 // Facebook
 const FACEBOOK = "/auth/facebook";
 const FACEBOOK_CALLBACK = "/auth/facebook/callback";
+
+// API
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 
 const routes = {
     home: HOME,
@@ -74,7 +75,9 @@ const routes = {
     gitHub: GITHUB,
     githubCallback: GITHUB_CALLBACK,
     facebook: FACEBOOK,
-    facebookCallback: FACEBOOK_CALLBACK
+    facebookCallback: FACEBOOK_CALLBACK,
+    api: API,
+    registerView: REGISTER_VIEW
 };
 
 export default routes;
