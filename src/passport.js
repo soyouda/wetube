dd219a5a3676d25515f5e894fa1ccd8f7d217578
soyouda <soyouda@gmail.com>
2019-03-19 13:38:42 +0900

    Change Authenticate Callback URL

--- src/passport.js
+++ src/passport.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import passport from "passport";
 import GithubStrategy from "passport-github";
 import FacebookStrategy from "passport-facebook";
@@ -5,12 +6,16 @@ import User from "./models/User";
 import {facebookLoginCallback, githubLoginCallback} from "./controllers/userController";
 import routes from "./routes";
 
+const DNS = `http://${process.env.AWS_DNS}:${process.env.PORT}`;
+
+dotenv.config();
+
 passport.use(User.createStrategy());
 passport.use(
     new GithubStrategy({
         clientID : process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `http://localhost:4000${routes.githubCallback}`
+        callbackURL: `${DNS}${routes.githubCallback}`
         },
         githubLoginCallback
     )
@@ -20,7 +25,7 @@ passport.use(
     new FacebookStrategy({
         clientID : process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
-        callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+        callbackURL: `${DNS}${routes.facebookCallback}`,
         profileFields: ['id', 'displayName', 'photos', 'email'],
         scope: ['public_profile', 'email']
     },
